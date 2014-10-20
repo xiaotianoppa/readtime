78a3f281309f52aa025ee6890e1560e81af7c76e
chx <18768113677@163.com>
2014-10-20 19:51:32 +0800

    add

---
+++ app/controllers/AdminBase.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import play.mvc.After;
+import play.mvc.Before;
+
+public class AdminBase extends CRUD{
+
+    private static final Logger log = LoggerFactory.getLogger(AdminBase.class);
+            
+    @Before
+    public static void startTime() {
+        log.info("Request For " + request.url + ":" + request.action + " Starts, ip: " + ControllerUtils.getRemoteIp());
+        request.args.put("_ts", System.currentTimeMillis());
+        
+        if(CheckUserLogin.checkHasRight()){
+            
+        }else{
+            ControllerUtils.redirectToQTNoAuth();
+        }
+       
+    }
+    
+    @After
+    public static void endTime() {
+        log.info("Action [" + request.url + "] took "
+                + (System.currentTimeMillis() - (Long) request.current().args.get("_ts")) + " ms");
+    }
+    
+}
--- app/controllers/UserAdmin.java
+++ app/controllers/UserAdmin.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import controllers.CRUD;
+import controllers.CRUD.For;
 import models.User;
 import play.mvc.With;
 
 @CRUD.For(User.class)
-public class UserAdmin extends CRUD {
+public class UserAdmin extends AdminBase {
     
 }
